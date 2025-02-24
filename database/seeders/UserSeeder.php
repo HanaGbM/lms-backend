911f5e029868d6a55c6925799ea00603d5d7b31f
Aman D <amandesalegnb@gmail.com>
2025-02-24 10:47:49 +0300

    user modified

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,21 +15,26 @@ class UserSeeder extends Seeder
     {
 
         $admin = User::updateOrCreate([
-            'name' => 'Admin',
             'email' => 'amandesalegnb@gmail.com',
             'phone' => '251963732919',
             'username' => 'admin',
+        ], [
+            'name' => 'Admin',
+            'email_verified_at' => now(),
+            'bod' => now()->subYears(40),
             'password' => bcrypt('password')
         ]);
 
         $admin->assignRole('Admin');
 
-
         $teacher = User::updateOrCreate([
-            'name' => 'Teacher',
             'email' => 'teacher@lms.com',
             'phone' => '251963732920',
             'username' => 'teacher',
+        ], [
+            'name' => 'Teacher',
+            'email_verified_at' => now(),
+            'bod' => now()->subYears(30),
             'password' => bcrypt('password')
         ]);
 
@@ -37,10 +42,14 @@ class UserSeeder extends Seeder
 
 
         $student = User::updateOrCreate([
-            'name' => 'Student',
             'email' => 'student@lms.com',
             'phone' => '251963732921',
             'username' => 'student',
+
+        ], [
+            'name' => 'Student',
+            'email_verified_at' => now(),
+            'bod' => now()->subYears(20),
             'password' => bcrypt('password')
         ]);
 
