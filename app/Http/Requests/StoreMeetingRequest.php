18cbe80ad4914b4a1ad8e46f4f9cf5b5ee31ec1c
Aman D <amandesalegnb@gmail.com>
2025-04-08 10:20:23 +0300

    discussion

--- app/Http/Requests/StoreMeetingRequest.php
+++ app/Http/Requests/StoreMeetingRequest.php
@@ -25,10 +25,11 @@ class StoreMeetingRequest extends FormRequest
             'title' => ['required', 'string', 'max:255'],
             'description' => ['required', 'string'],
             'url' => ['nullable', 'url'],
+            'all_day' => ['required', 'boolean'],
             'start_date' => ['required', 'date', 'before_or_equal:end_date', 'after_or_equal:today'],
             'end_date' => ['required', 'date', 'after_or_equal:start_date', 'after_or_equal:today'],
-            'start_time' => ['required', 'date_format:H:i', 'before_or_equal:end_time', 'after_or_equal:00:00'],
-            'end_time' => ['required', 'date_format:H:i', 'after_or_equal:start_time', 'before:23:59'],
+            'start_time' => ['required_if:all_day,false', 'date_format:H:i', 'before_or_equal:end_time', 'after_or_equal:00:00'],
+            'end_time' => ['required_if:all_day,false', 'date_format:H:i', 'after_or_equal:start_time', 'before:23:59'],
             'participants' => ['required', 'array'],
             'participants.*' => ['exists:users,id'],
         ];
